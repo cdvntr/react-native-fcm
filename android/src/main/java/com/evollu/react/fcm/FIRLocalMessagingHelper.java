3d24db5078ccfd51422f204030287010c9fb3adb
Okan KARAMAN <karamanokan@msn.com>
2018-03-26 18:30:02 +0300

    bring to front

--- android/src/main/java/com/evollu/react/fcm/FIRLocalMessagingHelper.java
+++ android/src/main/java/com/evollu/react/fcm/FIRLocalMessagingHelper.java
@@ -354,6 +354,11 @@ public class FIRLocalMessagingHelper {
                 WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
                 WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
 
+         mContext.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+                                        mContext.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+                                        mContext.getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
+                                        mContext.getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+
         mContext.startActivity(launchIntent);
     }
 }
